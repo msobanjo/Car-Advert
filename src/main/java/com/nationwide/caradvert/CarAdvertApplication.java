05b1cc16474c5c4fbd07383ca0b5ed70b0fc2f1b
Michael <msobanjo@hotmail.com>
2019-10-18 10:51:35 +0100

    Created Java project

---
+++ src/main/java/com/nationwide/caradvert/CarAdvertApplication.java
@@ -0,0 +1,13 @@
+package com.nationwide.caradvert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarAdvertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarAdvertApplication.class, args);
+	}
+
+}
